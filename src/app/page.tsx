027c7e92d40d1c03d780151728eeaf40a34bb69e
Roberto Costa <roberto.araujo339@gmail.com>
2023-11-06 22:37:34 -0300

    editorconfig and prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import { Stack } from '@mui/material';
 import Button from '@mui/material/Button';
 
-const Home = () => (
-  <Stack
-    direction="column"
-    gap={4}
-    width="100vw"
-    height="100vh"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Button variant="contained">START</Button>
-  </Stack>
-);
+const Home = () => {
+	return (
+		<Stack
+			direction="column"
+			gap={4}
+			width="100vw"
+			height="100vh"
+			justifyContent="center"
+			alignItems="center"
+		>
+			<Button variant="contained">START</Button>
+		</Stack>
+	);
+};
 
 export default Home;
