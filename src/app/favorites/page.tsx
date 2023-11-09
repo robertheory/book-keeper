53a00b108eab3b9bd8fb074238f6055413def44d
Roberto Costa <roberto.araujo339@gmail.com>
2023-11-08 22:21:38 -0300

    feature: favorite page listing books

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,3 +1,69 @@
-const Favorites = () => <h1>FAVORITES</h1>;
+'use client';
+
+import BookCard from '@/components/BookCard';
+import { getList } from '@/store/listsSlice';
+import { Stack, Typography } from '@mui/material';
+import Image from 'next/image';
+import { useSelector } from 'react-redux';
+
+const Favorites = () => {
+	const favorites = useSelector(getList('favorites'));
+
+	const isEmpty = favorites.length === 0;
+
+	return (
+		<Stack
+			width="100%"
+			direction="column"
+			justifyContent="flex-start"
+			alignItems="flex-start"
+			gap={4}
+		>
+			<Typography variant="h1" fontWeight="600" fontSize="2rem">
+				Favorite books
+			</Typography>
+
+			{isEmpty ? (
+				<Stack
+					width="100%"
+					direction="column"
+					justifyContent="center"
+					alignItems="center"
+					gap={2}
+				>
+					<Image
+						src="/not-found.webp"
+						width={300}
+						height={300}
+						alt="content not found"
+					/>
+					<Typography
+						variant="h3"
+						align="center"
+						fontFamily="cursive"
+						fontStyle="oblique"
+						fontWeight="800"
+						fontSize="2.5rem"
+					>
+						No favorites yet
+					</Typography>
+				</Stack>
+			) : (
+				<Stack
+					direction="row"
+					width="100%"
+					justifyContent="flex-start"
+					alignItems="flex-start"
+					gap={2}
+					flexWrap="wrap"
+				>
+					{favorites.map((book) => (
+						<BookCard key={book.id} book={book} />
+					))}
+				</Stack>
+			)}
+		</Stack>
+	);
+};
 
 export default Favorites;
